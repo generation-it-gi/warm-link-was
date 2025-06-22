f0daee574c627974778f44e7cf1366ee329e2f7f
Jsplix <jkjin112@gmail.com>
2025-06-23 01:32:04 +0900

    chore: set swagger configuration

--- src/main/java/kr/warmlink/infrastructure/config/SwaggerConfig.java
+++ src/main/java/kr/warmlink/infrastructure/config/SwaggerConfig.java
@@ -1,2 +1,23 @@
-package kr.warmlink.infrastructure.config;public class SwaggerConfig {
+package kr.warmlink.infrastructure.config;
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
+import org.springdoc.core.models.GroupedOpenApi;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@OpenAPIDefinition(
+        info = @Info(title = "WARM-LINK WAS", description = "WARM-LINK API 명세입니다.", version = "v1")
+)
+@Configuration
+public class SwaggerConfig {
+    @Bean
+    public GroupedOpenApi openApi() {
+        String[] paths = {"/**"};
+
+        return GroupedOpenApi.builder()
+                .group("WARM-LINK API v1")
+                .pathsToMatch(paths)
+                .build();
+    }
 }
