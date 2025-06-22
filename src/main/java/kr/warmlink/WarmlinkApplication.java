074a9d82b87e0bb829732241eef4a0169cb72224
Jsplix <jkjin112@gmail.com>
2025-06-22 18:36:13 +0900

    chore: project settings

---
+++ src/main/java/kr/warmlink/WarmlinkApplication.java
@@ -0,0 +1,13 @@
+package kr.warmlink;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WarmlinkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WarmlinkApplication.class, args);
+	}
+
+}
---
+++ src/test/java/kr/warmlink/WarmlinkApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.warmlink;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WarmlinkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
