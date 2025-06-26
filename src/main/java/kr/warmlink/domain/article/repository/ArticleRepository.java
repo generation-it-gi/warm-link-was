8d9b2d6b8b81abcacea046b1e293a17204e79ae6
Jsplix <jkjin112@gmail.com>
2025-06-27 00:23:34 +0900

    feat: add method for keyword search

--- src/main/java/kr/warmlink/domain/article/repository/ArticleRepository.java
+++ src/main/java/kr/warmlink/domain/article/repository/ArticleRepository.java
@@ -2,6 +2,13 @@ package kr.warmlink.domain.article.repository;
 
 import kr.warmlink.domain.article.entity.Article;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
 
 public interface ArticleRepository extends JpaRepository<Article, Long> {
+    @Query("SELECT a FROM article a WHERE a.title LIKE %:keyword%")
+    List<Article> findByKeyword(@Param("keyword") String keyword);
+
 }
